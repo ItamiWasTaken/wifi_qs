ef9a162d4067d0bbcee26d56ed18d2c7b2b60cf2
ItamiWasTaken <99271684+ItamiWasTaken@users.noreply.github.com>
2024-12-16 06:40:54 -0500

    Update WifiStateReceiver.java

--- app/src/main/java/n/eus/wifiqs/WifiStateReceiver.java
+++ app/src/main/java/n/eus/wifiqs/WifiStateReceiver.java
@@ -21,7 +21,7 @@ public class WifiStateReceiver extends BroadcastReceiver {
         final WifiManager wifiMan = context.getSystemService(WifiManager.class);
         final boolean isEnabled = wifiMan.isWifiEnabled();
         if (!isEnabled) {
-            listener.onWifiStateChanged(WifiState.DISABLED, int, -1);
+            listener.onWifiStateChanged(WifiState.DISABLED);
             return;
         }
         final WifiInfo wifiInfo = wifiMan.getConnectionInfo();
@@ -34,9 +34,9 @@ public class WifiStateReceiver extends BroadcastReceiver {
                 ssid = ssid.substring(1, ssid.length()-1);
             }
             final int rss = WifiManager.calculateSignalLevel(wifiInfo.getRssi(), 7);
-            listener.onWifiStateChanged(WifiState.CONNECTED, int, rss);
+            listener.onWifiStateChanged(WifiState.CONNECTED, rss);
         } else {
-            listener.onWifiStateChanged(WifiState.ENABLED, int, -1);
+            listener.onWifiStateChanged(WifiState.ENABLED);
         }
     }
 
